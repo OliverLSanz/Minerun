677cfacce1d3cd6fd6ce1d6fe2810ef51c0f1553
OliverSanz <urang.sowelu@gmail.com>
2015-11-20 00:55:04 +0100

    Minerun.java and Session.java updated.

--- Minerun/src/main/Minerun.java
+++ Minerun/src/main/Minerun.java
@@ -1,24 +1,32 @@
 package main;
 
-
 import java.util.HashMap;
 import telegramBot.*;
+import java.sql.*;
 
 public class Minerun {
-	
-	
-	public static void main(String[] args){
-		final String BOTID = "";
-		TelegramBot bot = new TelegramBot(BOTID);
-		HashMap<Integer, Session> currentSessions = new HashMap<Integer, Session>();
-		TelegramMsg msg;
-		
-		while(true){
-			while((msg = bot.nextMsg(0)) == null);
-			if(!currentSessions.containsKey(msg.getChat().getId())){
-				currentSessions.put(msg.getChat().getId(), new Session(msg.getChat()));
+
+	public static void main(String[] args) {
+		try {
+			final String URL = "", USER = "", PASS = "";
+			Connection connection = DriverManager.getConnection(URL, USER, PASS);
+
+			final String BOTID = "";
+			TelegramBot bot = new TelegramBot(BOTID);
+			HashMap<Integer, Session> currentSessions = new HashMap<Integer, Session>();
+			TelegramMsg msg;
+
+			while (true) {
+				while ((msg = bot.nextMsg(0)) == null)
+					;
+				if (!currentSessions.containsKey(msg.getChat().getId())) {
+					currentSessions.put(msg.getChat().getId(), new Session(msg.getChat(), connection));
+				}
+				currentSessions.get(msg.getChat().getId()).process(msg);
 			}
-			currentSessions.get(msg.getChat().getId()).process(msg);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
--- Minerun/src/main/Session.java
+++ Minerun/src/main/Session.java
@@ -1,18 +1,52 @@
 package main;
 
 import telegramBot.*;
+import java.sql.*;
 
 public class Session {
 	private int state;
 	private TelegramChat user;
+	private Connection db;
 	
-	public Session(TelegramChat user){
+	public Session(TelegramChat user, Connection db){
 		state = 0;
 		this.user = user;
+		this.db = db;
+		if(!userExistsInDb()){
+			dbExecute("INSERT INTO mineral (user_id) VALUES ("+ user.getId() +");");
+		}
 	}
 	
 	public void process(TelegramMsg msg){
 		// According to the msg and/or state, commit the appropriate actions
 	}
-
+	
+	private void dbExecute(String querry){
+		try {
+			db.createStatement().execute(querry);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private ResultSet dbExecuteQuerry(String querry){
+		try {
+			return db.createStatement().executeQuery(querry);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private boolean userExistsInDb(){
+		try {
+			return dbExecuteQuerry("SELECT " + user.getId() + " FROM mineral;").next();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
